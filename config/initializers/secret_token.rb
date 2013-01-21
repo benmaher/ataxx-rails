77b61d8953804c125acee1879c2a28996085a379
Ben Maher <maher@uchicago.edu>
2013-01-21 13:29:01 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtaxxRails::Application.config.secret_token = 'b41b5738e325f49152d15769ac25e9b764eae6302daea4b510bf061e14cf8e3b6735f9cff529d8f6cf63b663b194decdf25543e2b5e3e36397b05d44e839fc79'
